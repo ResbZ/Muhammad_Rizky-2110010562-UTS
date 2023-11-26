e5f6f4068dad650d0b950d79b06a873c4e096dd7
Muhammad Rizky <55248892+ResbZ@users.noreply.github.com>
2023-11-26 20:40:15 +0800

    Add files via upload

---
+++ java/MainClass.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Resb
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    
+    
+    
+    MainFrame myFrame = new MainFrame();
+    myFrame.init();
+    }
+}
---
+++ java/MainFrame.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Resb
+ */
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+
+public class MainFrame extends JFrame {
+    public void init() {
+        setTitle("Welcome");
+        setSize(450, 300);
+        setVisible(true);
+        
+        
+        
+    }
+}
+    
